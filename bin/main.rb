00f31100ecebb4f5c6848a90d35a1d9ef1e4a1f9
Omar Abd el Hameed <omarlabana@outlook.com>
2021-05-05 17:24:20 +0200

    Add basic player handshake

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+puts 'Welcome to Ruby\'s Tic-Tac-Toe'
+puts 'Please, Enter player 1 name:'
+player_1_name = gets.chomp
+puts 'Please, Enter player 2 name:'
+player_2_name = gets.chomp
+puts "#{player_1_name} will play with X and #{player_2_name} will play with O"
